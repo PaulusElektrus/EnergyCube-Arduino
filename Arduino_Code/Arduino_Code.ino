134358e4312ecfe370e1ff492f2b99d6d965980e
Paul Z <paul@zechs.de>
2023-05-24 17:57:27 +0200

    basic functionality, not working

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -6,68 +6,213 @@
 #define Relais_DC_to_WR     10
 #define Relais_WR_to_AC     11
 
+// PWM Pins
+#define PWM_NT 3
+#define PWM_DC 5
+
 // ADS1115
 #include<ADS1115_WE.h> 
 #include<Wire.h>
 #define I2C_ADDRESS 0x48
 ADS1115_WE adc = ADS1115_WE(I2C_ADDRESS);
 
-// PWM Output
-#define PWM_NT 3
-#define PWM_DC 5
-
 // Incoming Communication
 boolean newData = false;
 const byte numChars = 32;
 char receivedChars[numChars];
 char tempChars[numChars];
-char commandFromESP[numChars] = {0};
+
+// Incoming Data
+int commandFromESP = 0;
 int powerFromESP = 0;
 
 // Measurements
-String Status = "O";
+int status = 0;
 float uNT = 0.0;
 float uBatt = 0.0;
-float uWR = 0.0;
+float uKal = 0.0;
 float iBatt = 0.0;
 int bsPower = 0.0;
 
+// Status
+bool ntReady = false;
+bool ntSynced = false;
+bool dcReady = false;
+bool bsFull = false;
+bool bsEmpty = false;
+
+// PWM Status
+int pwmNt = 255;
+int pwmDc = 255;
+
+// Safety
+float maxUBatt = 49.2;
+float minUBatt = 44.4;
+float maxIBattCharging = -3.0;
+float maxbsPowerDischarging = 100;
+
 
 void setup(){
-    Serial.begin(115200);
+    pinMode(Relais_AC, OUTPUT);
+    pinMode(Relais_AC_to_NT, OUTPUT);
+    pinMode(Relais_NT_to_BT, OUTPUT);
+    pinMode(Relais_BT_to_DC, OUTPUT);
+    pinMode(Relais_DC_to_WR, OUTPUT);
+    pinMode(Relais_WR_to_AC, OUTPUT);
+    pinMode(PWM_NT, OUTPUT);
+    pinMode(PWM_DC, OUTPUT);
     Wire.begin();
     adc.init();
     adc.setVoltageRange_mV(ADS1115_RANGE_6144);
+    Serial.begin(115200);
 }
 
 
 void loop(){
-    measurement();
-    getCommand();
+    safetyCheck();
     control();
+    Serial.print("N");
 }
 
 
-void measurement() {
-    uNT = readChannel(ADS1115_COMP_0_GND);
-    uBatt = readChannel(ADS1115_COMP_1_GND);
-    uWR = readChannel(ADS1115_COMP_2_GND);
-    iBatt = readChannel(ADS1115_COMP_3_GND);
-    bsPower = uBatt * iBatt;
+void safetyCheck() {
+    measurement();
+    if (uBatt > maxUBatt) {
+        bsFull = true;
+        off();
+    }
+    if (uBatt < minUBatt) {
+        bsEmpty = true;
+        off();
+    }
+    if (iBatt < maxIBattCharging) {
+        off();
+        status = 3;
+    }
+    if (bsPower > maxbsPowerDischarging) {
+        off();
+        status = 3;
+    }
 }
 
 
-float readChannel(ADS1115_MUX channel) {
-  float voltage = 0.0;
-  adc.setCompareChannels(channel);
-  adc.startSingleMeasurement();
-  while(adc.isBusy()){}
-  voltage = adc.getResult_mV();
-  return voltage;
+void control() {
+    getCommand();
+    if (commandFromESP == 0) {
+        off();
+    }
+    else if (commandFromESP == 1 && bsFull == false) {
+        if (powerFromESP < 0) {
+            if (ntReady == false) {
+                activateNT();
+            }
+            if (ntSynced == false) { 
+                syncNT();
+            }
+            status = 1;
+            charge();
+        }   
+        else {
+            off();
+        }
+    }
+    else if (commandFromESP == 2 && bsEmpty == false) {
+        if (powerFromESP > 0) {
+            if (dcReady == false) {
+                activateDC();
+            }
+            status = 2;
+            discharge();
+        }   
+        else {
+            off();
+        }
+    }
+}
+
+
+void off() {
+    digitalWrite(Relais_AC, LOW);
+    digitalWrite(Relais_AC_to_NT, LOW);
+    digitalWrite(Relais_NT_to_BT, LOW);
+    digitalWrite(Relais_BT_to_DC, LOW);
+    digitalWrite(Relais_DC_to_WR, LOW);
+    digitalWrite(Relais_WR_to_AC, LOW);
+    status = 0;
+    ntReady = false;
+    ntSynced = false;
+    dcReady = false;
+    pwmNt = 255;
+    pwmDc = 255;
+}
+
+
+void activateNT() {
+    off();
+    digitalWrite(Relais_AC, HIGH);
+    digitalWrite(Relais_AC_to_NT, HIGH);
+    ntReady = true;
+}
+
+
+void syncNT() {
+    do {
+        analogWrite(PWM_NT, pwmNt);
+        delay(50);
+        measurement();
+        pwmNt = --pwmNt;
+    } while (uNT < uBatt);
+    ntSynced = true;
+}
+
+
+void charge() {
+    digitalWrite(Relais_NT_to_BT, HIGH);
+    while (powerFromESP < 0 && iBatt >= maxIBattCharging) {
+        pwmNt = --pwmNt;
+        analogWrite(PWM_NT, pwmNt);
+        delay(50);
+        getCommand();
+    }
+    while (powerFromESP > 0) {
+        pwmNt = ++pwmNt;
+        analogWrite(PWM_NT, pwmNt);
+        delay(50);
+        getCommand();
+    }
+}
+
+
+void activateDC() {
+    off();
+    digitalWrite(Relais_AC, HIGH);
+    digitalWrite(Relais_BT_to_DC, HIGH);
+    analogWrite(PWM_DC, pwmDc);
+    delay(5000);
+    dcReady = true;
+}
+
+
+void discharge() {
+    digitalWrite(Relais_DC_to_WR, HIGH);
+    while (powerFromESP > 0 && bsPower <= maxbsPowerDischarging) {
+        pwmNt = --pwmNt;
+        analogWrite(PWM_NT, pwmNt);
+        delay(50);
+        getCommand();
+    }
+    while (powerFromESP < 0) {
+        pwmNt = ++pwmNt;
+        analogWrite(PWM_NT, pwmNt);
+        delay(50);
+        getCommand();
+    }
 }
 
 
 void getCommand() {
+    measurement();
+    returnData();
     recvWithStartEndMarkers();
     if (newData == true) {
         strcpy(tempChars, receivedChars);
@@ -114,27 +259,38 @@ void parseData() {
 
     char * strtokIndx;
 
-    strtokIndx = strtok(tempChars,",");
-    strcpy(commandFromESP, strtokIndx);     
+    strtokIndx = strtok(tempChars, ",");
+    commandFromESP = atoi(strtokIndx);     
 
     strtokIndx = strtok(NULL, ",");
     powerFromESP = atof(strtokIndx);
-
-    returnData();
-}
-
-
-void control() {
-    bsPower = uBatt * iBatt;
 }
 
 
 void returnData() {
-    String outgoingData = "<" + Status + "," + uBatt + "," + iBatt + "," + bsPower + ">";
+    String outgoingData = "<" + String(status) + "," + uBatt + "," + iBatt + ">";
     Serial.print(outgoingData);
 }
 
 
-void control() {
-    
+void measurement() {
+    uNT = readChannel(ADS1115_COMP_0_GND);
+    uBatt = readChannel(ADS1115_COMP_1_GND);
+    uKal = readChannel(ADS1115_COMP_2_GND);
+    iBatt = readChannel(ADS1115_COMP_3_GND);
+    uNT = 45;
+    uBatt = 45;
+    uKal = 5;
+    iBatt = 1;
+    bsPower = uBatt * iBatt;
+}
+
+
+float readChannel(ADS1115_MUX channel) {
+  float voltage = 0.0;
+  adc.setCompareChannels(channel);
+  adc.startSingleMeasurement();
+  while(adc.isBusy()){}
+  voltage = adc.getResult_mV();
+  return voltage;
 }
